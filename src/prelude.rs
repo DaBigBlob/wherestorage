3092705f67c7dff92197f94e5c96bae59196ecd5
Person <localboxcrox@gmail.com>
2024-07-22 02:24:41 -0400

    stuff

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,26 +1,20 @@
 
 use std::fmt::Display;
 
-pub enum Error {
-    WithMessage(String),
-    UnderlyingError(String)
-}
+pub struct Error(String);
 
 #[allow(dead_code)]
 impl Error {
     pub fn from_err(e: impl Display) -> Self {
-        Error::UnderlyingError(e.to_string())
+        Self(format!("Underlying Error: {}", e.to_string()))
     }
 
-    pub fn from_str(s: &str) -> Self {
-        Error::WithMessage(s.to_string())
+    pub fn from_str(str: &str) -> Self {
+        Self(str.to_string())
     }
 
     pub fn to_string(self) -> String {
-        match self {
-            Self::UnderlyingError(s) => s,
-            Self::WithMessage(s) => s
-        }
+        self.0
     }
 }
 
