93e014351066fe19e66dff10fa0980dd6a953b8e
Person <localboxcrox@gmail.com>
2024-07-22 00:13:32 -0400

    stuff

--- src/speedtest_net/prelude.rs
+++ src/speedtest_net/prelude.rs
@@ -1,4 +1,6 @@
 
+use std::{fmt::Display, io::{stdout, Write}, process};
+
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum Error {
@@ -12,4 +14,30 @@ pub enum Error {
     Unimplemented
 }
 
+// pub enum Error {
+//     Message(String),
+//     Underlying(Error)
+// }
+
 pub type Result<T> = core::result::Result<T, Error>;
+
+// pub fn resolve_final<T: Display>(r: Result<T>) {
+//     let mut stdo = stdout().lock();
+//     match r {
+//         Ok(t) => {
+//             match stdo.write_all(format!("{}",t).as_bytes()) {
+//                 Ok(_) => (),
+//                 Err(_) => panic!("Cannot write to stdout.")
+//             }
+//             process::exit(1);
+//         },
+
+//         Err(_) => {
+//             match stdo.write_all(format!("").as_bytes()) {
+//                 Ok(_) => (),
+//                 Err(_) => panic!("Cannot write to stdout.")
+//             }
+//             process::exit(1);
+//         }
+//     }
+// }
