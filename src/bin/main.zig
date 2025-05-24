82d9bb89c1333d946c10ca3d8e007377b41daf1f
Person <localboxcrox@gmail.com>
2025-05-24 02:19:28 -0400

    stuff

--- src/bin/main.zig
+++ src/bin/main.zig
@@ -11,16 +11,18 @@ pub fn main() !void {
     const res = try http.Client.fetch(
         &client,
         .{
-            .location = .{ .url = "https://www.speedtest.net/api/results.php" },
+            .location = .{ .url = "https://api.hman.io/headerdebug" },
             .method = .POST,
             .headers = .{
                 .user_agent = .{ .override = "Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0" },
                 .accept_encoding = .{ .override = "gzip, deflate, br, zstd" },
                 .content_type = .{ .override = "application/json;charset=UTF-8" },
                 .authorization = .omit,
-                .connection = .{ .override = "keep-alive" },
+                .connection = .default,
                 .host = .omit,
             },
+            .keep_alive = true,
+            .payload = "{\"serverid\": 1897,\"ping\": 69,\"upload\": 69000,\"download\": 69000,\"hash\": \"098680718fcd24abc8bafcbd3a802ad1\"}",
         },
     );
     std.debug.print("{}\n", .{res.status});
