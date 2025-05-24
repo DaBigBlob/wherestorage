c51864b60c881730330a416767b2223e1825c840
Person <localboxcrox@gmail.com>
2025-05-24 00:54:26 -0400

    stuff

--- src/bin/main.zig
+++ src/bin/main.zig
@@ -1,4 +1,7 @@
+//! The main executable. Used to upload files directly.
+//!
 const std = @import("std");
 const lib = @import("wherestorage_lib");
+const testing = std.testing;
 
 pub fn main() !void {}
