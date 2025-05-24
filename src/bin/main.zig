3b89d0173c7d0cae6f85bc905f0a4d6c71ca049d
Person <localboxcrox@gmail.com>
2025-05-24 02:58:23 -0400

    stuff

--- src/bin/main.zig
+++ src/bin/main.zig
@@ -39,7 +39,7 @@ pub fn main() !void {
     );
     defer req.deinit();
 
-    req.transfer_encoding = .{ .content_length = payload.len };
+    // req.transfer_encoding = .{ .content_length = payload.len };
     try req.send();
     // var wtr = req.writer();
     try req.writeAll(payload);
