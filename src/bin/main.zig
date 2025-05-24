9650c2ab408fba791adadbdc076ac6a735387ab7
Person <localboxcrox@gmail.com>
2025-05-24 17:36:34 -0400

    stuff

--- src/bin/main.zig
+++ src/bin/main.zig
@@ -3,10 +3,30 @@
 const std = @import("std");
 const lib = @import("wherestorage_lib");
 const testing = std.testing;
+const http = std.http;
 
 pub fn main() !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-    _ = allocator;
+    var _gpa = std.heap.DebugAllocator(.{}).init;
+    defer _ = _gpa.deinit();
+    const gpa = _gpa.allocator();
+
+    // var client = http.Client{ .allocator = gpa.allocator() };
+
+    const uri = comptime try std.Uri.parse("https://api.hman.io/headerdebug");
+    std.debug.print("uri = {}\n", .{uri});
+
+    const payload = try std.fmt.allocPrint(
+        gpa,
+        "{any}", //"{{\"serverid\":{d},\"ping\":{d},\"upload\":{d},\"download\":{d},\"hash\":\"{s}\"}}",
+        .{
+            1897,
+            // 69,
+            // 69690,
+            // 69690,
+            // "098680718fcd24abc8bafcbd3a802ad1",
+        },
+    );
+    defer gpa.destroy(payload);
+
+    std.debug.print("payload = {any}\n", .{payload});
 }
